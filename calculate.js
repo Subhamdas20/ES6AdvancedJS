6ebce8b3b1f713dcb94696bb09bf35cea3f93531
Subhamdas20 <Subham21.sd@gmail.com>
2021-12-23 11:51:42 +0530

    Added : ES6 default params

---
+++ calculate.js
@@ -0,0 +1,7 @@
+const calculateArea = function(height=50, width=60) {  
+   height =  height;
+    width = width ;
+    return height*width;
+ }
+
+ console.log(calculateArea(80,90))
---
+++ src/Node.js
@@ -0,0 +1,5 @@
+let greet = (name="Sir/Madam")=>{
+    console.log(`Hello ${name}. Good morning and have a nice day `)
+}
+
+greet("subham")
