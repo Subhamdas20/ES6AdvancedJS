cd6e24d8e8eaa737c7bb6fed5e9f60b93249d4a9
Subhamdas20 <Subham21.sd@gmail.com>
2021-12-23 12:02:34 +0530

    Added : literals

--- calculate.js
+++
@@ -1,7 +0,0 @@
-const calculateArea = function(height=50, width=60) {  
-   height =  height;
-    width = width ;
-    return height*width;
- }
-
- console.log(calculateArea(80,90))
--- src/Node.js
+++ src/Node.js
@@ -1,5 +1,9 @@
-let greet = (name="Sir/Madam")=>{
+let greet = (name = "Sir/Madam") => {
     console.log(`Hello ${name}. Good morning and have a nice day `)
 }
 
+let literal = (firstName, lastName) => {
+    console.log(`Hello ${firstName} ${lastName} ..How are you ? `)
+}
 greet("subham")
+literal("Subham","Das")
